1a06d8c744f077aa88d545ff33135581e748f08b
Wenjie <wuwenjiegogo@gmail.com>
2021-02-24 11:06:00 +0100

    magit setup, format early-init

--- elisp/early-init.el
+++ elisp/early-init.el
@@ -4,23 +4,42 @@
 (load "~/.emacs.d/config.el")
 ;;(require 'basic-ui-setup)
 (require 'package-init)
-(require 'theme)
-;;(require 'company-setup)
-(require 'smartparens-setup)
-(require 'evil-setup)
-(require 'helm-setup)
-(require 'which-key-setup)
-(require 'general-setup)
-(require 'winum-setup)
-;;(require 'dashboard-setup)
-(require 'advance-ui-setup)
-;;(require 'mode-line-setup)
-(require 'yasnippet-setup)
-(require 'treemacs-setup)
-(require 'latex-setup)
-(require 'org-setup)
-(require 'command-log-setup)
-(require 'path-setup)
-(require 'lsp-setup)
+
+(setq setup-list '(
+		   theme
+		   smartparens-setup
+		   evil-setup
+		   helm-setup
+		   which-key-setup
+		   general-setup
+		   winum-setup
+		   advance-ui-setup
+		   yasnippet-setup
+		   treemacs-setup
+		   latex-setup
+		   org-setup
+		   command-log-setup
+		   path-setup
+		   lsp-setup
+		   magit-setup
+		   ))
+(dolist (setup setup-list)
+  (require setup))
+
+;;(require 'theme)
+;;(require 'smartparens-setup)
+;;(require 'evil-setup)
+;;(require 'helm-setup)
+;;(require 'which-key-setup)
+;;(require 'general-setup)
+;;(require 'winum-setup)
+;;(require 'advance-ui-setup)
+;;(require 'yasnippet-setup)
+;;(require 'treemacs-setup)
+;;(require 'latex-setup)
+;;(require 'org-setup)
+;;(require 'command-log-setup)
+;;(require 'path-setup)
+;;(require 'lsp-setup)
 
 (provide 'early-init)
--- elisp/magit-setup.el
+++ elisp/magit-setup.el
@@ -2,4 +2,14 @@
 (global-leader
   "g" '(:ignore t :which-key "git")
   "gg" '(magit-status :which-key "status")
+  "gs" '(magit-stage-file :which-key "stage")
+  "gu" '(magit-unstage-file :which-key "unstage")
   )
+(dolist (state '(normal motion))
+  (evil-define-key state with-editor-mode-map
+    (concat normal-major-mode-leader-key normal-major-mode-leader-key) 'with-editor-finish
+    (concat normal-major-mode-leader-key "c") 'with-editor-finish
+    (concat normal-major-mode-leader-key "k") 'with-editor-cancel
+    )
+  )
+(provide 'magit-setup)
