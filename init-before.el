d2e0250a673423312d5b55ac12927b77337f8d7b
Wenjie <wuwenjiegogo@gmail.com>
2021-03-01 12:32:28 +0100

    fix leader key for winum

--- init-before.el
+++ init-before.el
@@ -170,10 +170,7 @@
 (use-package winum
   :config
   (winum-mode)
-  (general-define-key
-   :states '(normal visual insert emacs)
-   :prefix "SPC"
-   :non-normal-prefix "M-m"
+  (global-leader
    "1" '(winum-select-window-1 :which-key "window 1")
    "2" '(winum-select-window-2 :which-key "window 2")
    "3" '(winum-select-window-3 :which-key "window 3")
